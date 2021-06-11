0e02c2c502f047e01d3d13c06380698ab12b91e8
Minchao Duclos <chaohg@hotmail.com>
2021-06-10 22:08:13 -0700

    update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const rect = require ('./rectangle.js')
 function solveRect(l, w) {
     console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
     if (l <= 0 || w <= 0) {
-        console.log(`Rectangle dimensions must be greater than zero. Received: $(l), $(w)`);
+        console.log(`Rectangle dimensions must be greater than zero. Received: ${l}, ${w}`);
     } else {
         console.log(`Area of rectangle: ${rect.area(l, w)}`);
         console.log(`Perimeter of rectangle: ${rect.perimeter(l, w)}`);
@@ -14,4 +14,4 @@ function solveRect(l, w) {
     solveRect(2, 4)
     solveRect(3, 5)
     solveRect(0, 5)
-    solveRect(5, 3)
+    solveRect(5, -3)
